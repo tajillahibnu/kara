a774c0c29ce39a5035cc04e8f1e566c9fd07e832
tajillah ibnu <tajillah@tajillahs-MacBook-Air.local>
2025-02-13 23:33:25 +0700

    fix[pkl][priode]:perbaikan form dan label

--- Modules/Pkl/resources/views/layouts/plugins.blade.php
+++ Modules/Pkl/resources/views/layouts/plugins.blade.php
@@ -1,33 +1,34 @@
 <!-- Core JS -->
 <!-- build:js assets/vendor/js/core.js -->
 
-<script src="../../assets/vendor/libs/jquery/jquery.js"></script>
-<script src="../../assets/vendor/libs/popper/popper.js"></script>
-<script src="../../assets/vendor/js/bootstrap.js"></script>
-<script src="../../assets/vendor/libs/node-waves/node-waves.js"></script>
-<script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-<script src="../../assets/vendor/libs/hammer/hammer.js"></script>
-<script src="../../assets/vendor/libs/i18n/i18n.js"></script>
-<script src="../../assets/vendor/libs/typeahead-js/typeahead.js"></script>
-<script src="../../assets/vendor/js/menu.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/jquery/jquery.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/popper/popper.js"></script>
+<script src="{{asset('/')}}assets/vendor/js/bootstrap.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/node-waves/node-waves.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/hammer/hammer.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/i18n/i18n.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/typeahead-js/typeahead.js"></script>
+<script src="{{asset('/')}}assets/vendor/js/menu.js"></script>
 
 <!-- endbuild -->
 
 <!-- Vendors JS -->
-<script src="../../assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
 <!-- Flat Picker -->
-<script src="../../assets/vendor/libs/moment/moment.js"></script>
-<script src="../../assets/vendor/libs/flatpickr/flatpickr.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/moment/moment.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/flatpickr/flatpickr.js"></script>
 <!-- Form Validation -->
-<script src="../../assets/vendor/libs/@form-validation/popular.js"></script>
-<script src="../../assets/vendor/libs/@form-validation/bootstrap5.js"></script>
-<script src="../../assets/vendor/libs/@form-validation/auto-focus.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/@form-validation/popular.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/@form-validation/bootstrap5.js"></script>
+<script src="{{asset('/')}}assets/vendor/libs/@form-validation/auto-focus.js"></script>
 <script src="{{asset('/')}}assets/vendor/libs/sweetalert2/sweetalert2.js"></script>
 <script src="{{asset('/')}}assets/vendor/libs/toastr/toastr.js"></script>
 <script src="{{asset('/')}}assets/vendor/libs/select2/select2.js"></script>
 
 
 <!-- Main JS -->
-<script src="../../assets/js/main.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script src="{{asset('/')}}assets/js/main.js"></script>
+<script src="{{asset('/')}}assets/vendor/axios.min.js"></script>
+<!-- <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> -->
 <script src="{{asset('/')}}assets/custom/app.js"></script>
